1d351d5fee7bc4f5089881801988f140b581ed3d
Joy <joy8zhang@gmail.com>
2018-06-27 15:22:42 -0700

    hello world

---
+++ cppTutorialDerekBanas/cmake-build-debug/CMakeFiles/cppTutorialDerekBanas.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppTutorialDerekBanas.dir/main.cpp.o"
+  "cppTutorialDerekBanas.pdb"
+  "cppTutorialDerekBanas"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppTutorialDerekBanas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cppTutorialDerekBanas/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // cout and other std functions
+#include <vector>
+#include <string>
+#include <fstream> // IO capabilities
+
+using namespace std; // avoid typing std all times
+int main() {
+    cout << "Hello, World!" << endl;
+
+    
+    return 0;
+}
