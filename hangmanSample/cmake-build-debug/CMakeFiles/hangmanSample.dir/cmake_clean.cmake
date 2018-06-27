7bc6433774f13e11b123d8823c7be60344338bf7
Joy <joy8zhang@gmail.com>
2018-06-27 14:15:31 -0700

    hangman code sample

---
+++ hangmanSample/cmake-build-debug/CMakeFiles/hangmanSample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hangmanSample.dir/main.cpp.o"
+  "hangmanSample.pdb"
+  "hangmanSample"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hangmanSample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
