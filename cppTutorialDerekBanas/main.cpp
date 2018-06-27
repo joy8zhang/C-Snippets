8c5f6a3cabbf19137654f13dbe759bf032711b86
Joy <joy8zhang@gmail.com>
2018-06-27 15:31:29 -0700

    variables

--- cppTutorialDerekBanas/main.cpp
+++ cppTutorialDerekBanas/main.cpp
@@ -7,6 +7,18 @@ using namespace std; // avoid typing std all times
 int main() {
     cout << "Hello, World!" << endl;
 
-    
+    const double PI = 3.1415;
+    bool isHappy = false;
+    float favNum = 4.2562351;
+
+    cout << "Favorite Number " << favNum << endl; // endl terminates line
+    cout << "Size of float " << sizeof(favNum) << endl; // sizeOf returns size in bytes
+
+    int largestInt = 2147483647;
+    cout << "Largest int " << largestInt << endl;
+
+    int largestInt1 = 2147483648;
+    cout << "Largest int " << largestInt1 << endl;
+
     return 0;
 }
